d0b79d596c88122bbdf9849a21908ff471756768
eugeniosegala <eugenio.segala@hotmail.it>
2020-05-07 13:03:41 +0100

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # queue-manager
-A fake queue manager for testing purposes
+_simply test task manages_
+
+Note: I decided to use "Promises" to simulate a realistic situation.
+
+Basically, `reduce` within `getProcessingPage` will solve the task sequentially and based on the output (`resolve()` or `reject()`) the queue will proceed or will be interrupted.
+
+To start the function, simply pass an array of states to the `getProcessingPage` function.
+
+Although not necessary, I logged the "processing" state and I passed multiple states for testing purposes only.
+
+```
+// states example
+var tasks = [
+  { state: 'processing' },
+  { state: 'error', errorCode: 'NO_STOCK' },
+  { state: 'success' },
+];
+
+getProcessingPage(tasks);
+```
+
+**How to run it locally:** `node app.js`
+<br>
+**JS Bin:** https://jsbin.com/wunosaveki/1/edit?js,console,output
---
+++ app.js
@@ -0,0 +1,38 @@
+
+const errorCodeDictionary = {
+  'NO_STOCK': 'No stock has been found',
+  'INCORRECT_DETAILS': 'Incorrect details have been entered',
+};
+
+const jobManager = (currentTask) => {
+  return () => {
+    return new Promise((resolve) => {
+      if (currentTask.state === "processing") { // demo purposes
+        console.log(currentTask);
+        resolve();
+      } else {
+        setTimeout(() => {
+          console.log({
+            title: currentTask.state === "error" ? 'Error page' : 'Order complete',
+            message: errorCodeDictionary[currentTask.errorCode] || null
+          });
+          // reject() will stop the queue
+          resolve();
+        }, 2000);
+      }
+    })
+  }
+};
+
+const getProcessingPage = (data) => {
+  return data.reduce((acc, currentTask) => acc.then(jobManager(currentTask)), Promise.resolve());
+};
+
+// example list
+const tasks = [
+  { state: 'processing' },
+  { state: 'error', errorCode: 'NO_STOCK' },
+  { state: 'success' },
+];
+
+getProcessingPage(tasks);
